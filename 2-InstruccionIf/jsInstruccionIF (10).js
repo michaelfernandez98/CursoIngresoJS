8b01d3d21ee90d5b74d1a7ae4ea80df70250e8f7
michaelfernandez98 <36193708+michaelfernandez98@users.noreply.github.com>
2018-02-14 11:16:31 -0300

    14/02

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,24 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var nota;
+	nota=Math.floor((Math.random()*(10))+1);
+	console.log(nota);
 
+	if (nota>8) 
+	{
+		alert("La nota es: "+nota+" ¡Excelente!");
+	}
+	else
+	{
+		if (nota>3) 
+		{
+			alert("La nota es: "+nota+" ¡Aprobó!");
+		}
+		else 
+		{
+			alert("La nota es: "+nota+" Vamos la proxima se puede");
+		}
+		
+	}
 }//FIN DE LA FUNCIÓN
