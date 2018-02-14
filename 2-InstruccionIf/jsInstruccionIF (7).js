c24b81e6481bba3b3ab16f1d06047d100838f6ca
michaelfernandez98 <36193708+michaelfernandez98@users.noreply.github.com>
2018-02-14 09:22:15 -0300

    14/02

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -2,7 +2,7 @@ function Mostrar()
 {
 //tomo la edad  
 
-	var edad
+	var edad;
 	var estado;
 
 	edad=parseInt(document.getElementById('edad').value);
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -2,12 +2,12 @@ function Mostrar()
 {
 //tomo la edad  
 
-	var edad
-	var estado
+	var edad;
+	var estado;
 
 	edad=parseInt(document.getElementById('edad').value);
 	estado=document.getElementById('estadoCivil').value;
-	
+
 	if (edad<18&&estado!="Soltero")
 	 {}
 	else
