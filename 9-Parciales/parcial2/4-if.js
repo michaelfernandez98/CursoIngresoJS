c7e69c0460076f25466abbe7141bb2a579c4c8ba
michaelfernandez98 <36193708+michaelfernandez98@users.noreply.github.com>
2018-02-28 12:05:23 -0300

    28/02

--- 9-Parciales/parcial2/4-if.js
+++ 9-Parciales/parcial2/4-if.js
@@ -1,7 +1,46 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 4-if");
-	
+	//alert("Funciona 4-if");
+	//no usar while no validar
+	var numero1;
+	var numero2;
+	var multiplicacion;
+	var resta;
+	var concatenar;
+	var total;
+	var par;
+	//se ingresan dos numeros si son iguales se concatenan si el mayor es el primero se multiplica de lo contrario  se restan 
+	numero1=prompt("ingrese primer numero");
+	numero2=prompt("ingrese segundo numero");
+
+	if (numero1==numero2) 
+	{
+		concatenar=numero1+numero2;
+		total=concatenar;
+	}
+	else
+	{
+		numero1=parseInt(numero1);
+		numero2=parseInt(numero2);
+
+		if (numero1>numero2) 
+		{
+			multiplicacion=numero1*numero2;
+			total=multiplicacion;
+			if (multiplicacion%2==0) 
+			{
+				par="es par";
+			}
+		}
+		else
+		{
+			resta=numero1-numero2;
+			total=resta;
+		}
+	}
+	alert("el resultado es: "+total)
+
 }
 
+// si el resultado de la multiplicacion es par agregar la palabra es par al lado del resultado;
--- 9-Parciales/parcial2/8-iteraciones.js
+++ 9-Parciales/parcial2/8-iteraciones.js
@@ -1,7 +1,64 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 8-iteraciones");
-	
-}
+	//alert("Funciona 8-iteraciones");
+/*se ingresa hasta que el usuario quiera el nombre de un animal, 
+el peso del mismo, validar q sea mayor a cero, 
+la temperatura del habitat de ese animal, validar entre -40 y 40, 
+informar la cantidad de temperaturas pares la cantidad de temperaturas impares, 
+el nombre del animal mas pesado, el nombre del  animal menos pesado, 
+la cantidad de animales que viven en habitat de temperatura menores a 0 inclusive, 
+el promedio del peso de todos los animales,  la temperatura maxima y la minima.*/
+	var peso;
+	var nombre;
+	var contador=0;
+	var acumulador=0;
+	var temperaturadelhabitat;
+	var temperaturamaxima;
+	var temperaturaminima;
+	var respuesta="si";
+	var promedio;
+	var cantidadtemppar=0;
+	var cantidadtempimpar=0
 
+	while(respuesta!="no")
+	{
+		contador++;
+		nombre=prompt("Ingrese el nombre el animal");
+//#################################################################################################################
+		peso=prompt("Ingrese el peso del animal");
+		while(peso<1||isNaN(peso))
+		{
+			peso=prompt("ERROR Ingrese el peso del animal")
+			acumulador=acumulador+peso;			
+		}
+//#################################################################################################################
+
+		temperatura=prompt("Ingrese la temperatura del habitat del animal:");
+		temperatura=parseInt(temperatura);
+		while(temperatura>40||temperatura<-40)
+		{
+			temperatura=prompt("ERROR Ingrese correctamente la temperatura del habitat");
+			temperatura=parseInt(temperatura);
+		}
+		if (temperatura%2==0&&numero!=0) 
+		{
+			cantidadtempimpar++;
+		}
+		else{
+			if (temperatura%2!=0) 
+			{
+				cantidadtempimpar++;
+			}
+		}
+
+     respuesta=prompt("si quiere continuar ingrese 'no' de lo contrario pulse enter")
+
+
+
+	}
+
+document.write(temperatura)
+
+
+}
